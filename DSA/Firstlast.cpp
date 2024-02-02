370235d186d326532b2388ba85f1fdb8144d47d5
Saurabh <spssgzp4@gmail.com>
2024-02-02 22:57:30 +0530

    Firstoccurence

---
+++ DSA/Firstlast.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int firstocc(int arr[], int n, int key) {
+    int s = 0;
+    int e = n - 1;
+    int ans = -1;
+
+    while (s <= e) {
+        int mid = s + (e - s) / 2;
+
+        if (arr[mid] == key) {
+            ans = mid;
+            e = mid - 1;
+        } else if (key > arr[mid]) {
+            s = mid + 1;
+        } else {
+            e = mid - 1;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    int even[] = {2, 4, 6, 8, 8, 8, 8, 10};
+    int result = firstocc(even, 8, 8);
+    cout << "First occurrence is " << result;
+
+    return 0;
+}
